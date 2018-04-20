724cfcfe39829123a64c66b5890f621be46e1b4e
HeoH <h0h6h2h5@gmail.com>
2018-04-20 22:48:26 +0900

    Add playerName attribute to Player

--- src/test/java/com/cnu/blackjack/PlayerTest.java
+++ src/test/java/com/cnu/blackjack/PlayerTest.java
@@ -7,10 +7,14 @@ import static org.junit.Assert.assertEquals;
 public class PlayerTest {
 
     @Test
-    public void 플레이어는_시드머니를_가지고_생성할수있다() {
-        Player player = new Player(5000);
+    public void 플레이어는_이름과_시드머니를_가지고_생성할수있다() {
+        Player player = new Player("BlackJackKing", 5000);
+
+        String playerName = player.getPlayerName();
+        assertEquals(playerName, "BlackJackKing");
+
         int balance = player.getBalance();
-        assertEquals(5000, balance);
+        assertEquals(balance, 5000);
     }
 
     @Test
