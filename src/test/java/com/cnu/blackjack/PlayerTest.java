452f0e25a4cb22f3810fa142db7ff9fd521e9fe4
HeoH <h0h6h2h5@gmail.com>
2018-04-21 10:01:29 +0900

    Fix constructor

--- src/test/java/com/cnu/blackjack/PlayerTest.java
+++ src/test/java/com/cnu/blackjack/PlayerTest.java
@@ -19,7 +19,7 @@ public class PlayerTest {
 
     @Test
     public void 플레이어는_배팅을_할수있다() {
-        Player player = new Player(5000);
+        Player player = new Player("BlackJackKing", 5000);
         player.placeBet(3000);
         int balance = player.getBalance();
         assertEquals(balance, 2000);
@@ -27,12 +27,12 @@ public class PlayerTest {
 
     @Test(expected = NotEnoughBalanceException.class)
     public void 플레이어는_가진돈보다_많이_배팅할수없다() {
-        Player player = new Player(5000);
+        Player player = new Player("BlackJackKing", 5000);
         player.placeBet(10000);
     }
 
     public void 플레이어는_상금을_받을_수_있다() {
-        Player player = new Player(5000);
+        Player player = new Player("BlackJackKing", 5000);
         player.receiveReward(3000);
         int balance = player.getBalance();
         assertEquals(balance, 8000);
