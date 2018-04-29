d09d632e08adfd7c6d0f413f6147cbec3f664d7a
P_lang <sedongwhan@naver.com>
2018-04-29 16:21:33 +0900

    player & playertest commit

--- src/main/java/com/cnu/blackjack/Player.java
+++ src/main/java/com/cnu/blackjack/Player.java
@@ -2,6 +2,8 @@ package com.cnu.blackjack;
 
 import lombok.Data;
 
+import java.util.Scanner;
+
 @Data
 public class Player {
     private int balance;
@@ -11,11 +13,47 @@ public class Player {
         this.balance = balance;
     }
 
+    public Player(int balance, PlayerController playerController) {
+        this.balance = balance;
+        if(playerController.equals("null")){
+            ComPlayerController comPlayerController  = new ComPlayerController();
+            comPlayerController.playGame();
+        }
+        else{
+            new UserPlayerController();
+        }
+    }
+
     public void placeBet(int bet) {
         if (bet > this.balance) {
             throw new NotEnoughBalanceException();
         }
         this.balance -= bet;
         this.currentBet = bet;
+    }
+
+    public String getPlayerName() {
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+
+    public PlayerController getController() {
+        return new PlayerController();
+    }
+
+    public Hand getHand() {
+        return new Hand();
+    }
+
+    public void receiveReward() {
+        if(winGame()){
+            balance += (currentBet*1.5);
+        }
+        else if(winGamewithBlackjack()){
+            balance += (currentBet*2);
+        }
+        else{
+            balance += 0;
+        }
     }
 }
--- src/test/java/com/cnu/blackjack/PlayerTest.java
+++ src/test/java/com/cnu/blackjack/PlayerTest.java
@@ -9,7 +9,7 @@ public class PlayerTest {
 
     @Test
     public void 플레이어는_이름과_시드머니를_가지고_생성할수있다() {
-        Player player = new Player("BlackJackKing", 5000);
+        Player player = new Player(5000);
 
         String playerName = player.getPlayerName();
         assertEquals(playerName, "BlackJackKing");
@@ -20,7 +20,7 @@ public class PlayerTest {
 
     @Test
     public void 플레이어는_기본적으로_Com컨트롤러를_가진다() {
-        Player player = new Player("BlackJackKing", 5000);
+        Player player = new Player(5000);
 
         PlayerController controller = player.getController();
         assertEquals(controller.getClass(), ComPlayerController.class);
@@ -28,7 +28,7 @@ public class PlayerTest {
 
     @Test
     public void 플레이어는_컨트롤러를_지정해서_생성할_수_있다() {
-        Player player = new Player("BlackJackKing", 5000, new UserPlayerController());
+        Player player = new Player(5000, new UserPlayerController());
 
         PlayerController controller = player.getController();
         assertEquals(controller.getClass(), UserPlayerController.class);
@@ -36,7 +36,7 @@ public class PlayerTest {
 
     @Test
     public void 플레이어의_컨트롤러가_null이라면_Com컨트롤러를_가진다() {
-        Player player = new Player("BlackJackKing", 5000, null);
+        Player player = new Player(5000, null);
 
         PlayerController controller = player.getController();
         assertEquals(controller.getClass(), ComPlayerController.class);
@@ -44,7 +44,7 @@ public class PlayerTest {
 
     @Test
     public void 플레이어는_배팅을_할수있다() {
-        Player player = new Player("BlackJackKing", 5000);
+        Player player = new Player(5000);
         player.placeBet(3000);
         int balance = player.getBalance();
         assertEquals(balance, 2000);
@@ -52,22 +52,21 @@ public class PlayerTest {
 
     @Test(expected = NotEnoughBalanceException.class)
     public void 플레이어는_가진돈보다_많이_배팅할수없다() {
-        Player player = new Player("BlackJackKing", 5000);
+        Player player = new Player(5000);
         player.placeBet(10000);
     }
 
     @Test
     public void 플레이어는_상금을_받을_수_있다() {
-        Player player = new Player("BlackJackKing", 5000);
-        player.receiveReward(3000);
+        Player player = new Player(5000);
+        player.receiveReward();
         int balance = player.getBalance();
         assertEquals(balance, 8000);
     }
 
     @Test
     public void 플레이어는_핸드를_가진다() {
-        Player player = new Player("BlackJackKing", 5000);
-
+        Player player = new Player(5000);
         Hand playerHand = player.getHand();
         assertNotNull(playerHand);
     }
