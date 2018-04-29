5eb4dd514715d2ccacd6f495d85e37da668a5dd3
HeoH <h0h6h2h5@gmail.com>
2018-04-29 17:04:04 +0900

    Implement in_playerBalance()

--- src/main/java/com/cnu/blackjack/AppIO.java
+++ src/main/java/com/cnu/blackjack/AppIO.java
@@ -1,6 +1,7 @@
 package com.cnu.blackjack;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class AppIO {
@@ -21,6 +22,19 @@ public class AppIO {
         scan.close();
 
         return answer;
+    }
+
+    private static int in_int(String message, int min, int max) throws NumberFormatException, NumberOutOfBoundsException {
+        String answer = AppIO.in_line(message);
+        int value = Integer.parseInt(answer);
+
+        if (value < min) {
+            throw new NumberOutOfBoundsException();
+        }
+        if (value > max) {
+            throw new NumberOutOfBoundsException();
+        }
+        return value;
     }
 
     private static boolean in_confirm(String message) throws YesOrNoException {
@@ -55,6 +69,23 @@ public class AppIO {
             }
             catch (YesOrNoException e) {
                 AppIO.out_errorMessage("잘못된 입력입니다.");
+            }
+        }
+    }
+
+    public static int in_playerBalance() {
+        while (true) {
+            try {
+                int minBalance = 0;
+                int maxBalance = Integer.MAX_VALUE;
+                int balance = AppIO.in_int("플레이어의 소지금을 입력하십시오: ", minBalance, maxBalance);
+                return balance;
+            }
+            catch (NumberFormatException e) {
+                AppIO.out_errorMessage("잘못된 입력입니다.");
+            }
+            catch (NumberOutOfBoundsException e) {
+                AppIO.out_errorMessage("소지금은 0보다 작을 수 없습니다.");
             }
         }
     }
