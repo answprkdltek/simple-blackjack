122f0ce6fc2ba1628bae01910275cc5c810b7ff2
Usung <edonc700@gmail.com>
2018-04-29 21:46:53 +0900

    ComPlayerController & UsedCards

--- src/main/java/com/cnu/blackjack/ComPlayerController.java
+++ src/main/java/com/cnu/blackjack/ComPlayerController.java
@@ -2,14 +2,91 @@ package com.cnu.blackjack;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class ComPlayerController extends PlayerController{
+    private int previousBet = 0;
+    private int previousBalance = 0;
 
     public int placeBet(int balance, int minBet) {
-        return 0;
+        int Bet = martinGale(balance);
+        return Bet > minBet ? Bet : minBet;
+    }
+
+    private int Kelly(int balance){
+        Double edge = 0.1;
+        Double result = balance*edge;
+        return result.intValue();
+    }
+
+    private int martinGale(int balance){
+        int Bet = 0;
+        if(!amIWinAtThePreviousGame(balance)){
+            Bet = previousBet *2;
+        }else{
+            Bet = previousBet;
+        }
+        previousBet = Bet;
+        return Bet;
+    }
+
+    private Boolean amIWinAtThePreviousGame(int balance){
+        if (previousBalance-previousBet + (previousBet*1.5)== balance ||
+                previousBalance-previousBet + (previousBet * 2) ==balance){
+            previousBalance = balance;
+            return true;
+        }else{
+            previousBalance = balance;
+            return false;
+        }
     }
 
     public boolean wannaHit(Hand hand) {
+        int currentScore = Evaluator.checkScore(hand);
+        if(currentScore < 12){
+            return true;
+        }
+        return percentageOfBlackjack(currentScore) > 0.1 ? true : false;
+    }
+
+    private double percentageOfBlackjack(int currentScore){
+        List<Card> leftDeck = new ArrayList<Card>();
+        for (int j = 0; j < 3; j++) {
+            for (int i = 1; i <= 13; i++) {
+                for (Suit suit: Suit.values()) {
+                    Card card = new Card(i, suit);
+                    if (!isUsedCard(card)){
+                        leftDeck.add(card);
+                    }
+                }
+            }
+        }
+
+        int neededScore = 21 - currentScore;
+        int numberOfHasNeededScoreCards = 0;
+
+        for (int i=0; i<leftDeck.size();i++){
+            int getRank = leftDeck.get(i).getRank();
+            if (getRank > 11){
+                getRank = 10;
+            }
+            if (getRank == neededScore){
+                numberOfHasNeededScoreCards++;
+            }
+        }
+
+        return numberOfHasNeededScoreCards / leftDeck.size();
+    }
+
+    private boolean isUsedCard(Card card){
+        List<Card> usedCardList = UsedCards.getUsedCardList();
+        for(int i=0; i< usedCardList.size();i++){
+            if (usedCardList.get(i).equals(card)){
+                return true;
+            }
+        }
         return false;
     }
 }
