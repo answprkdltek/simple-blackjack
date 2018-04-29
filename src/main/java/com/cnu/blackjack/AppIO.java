a5792fcd1501c9029deb6be798a8f526cbd3ff5b
HeoH <h0h6h2h5@gmail.com>
2018-04-29 16:32:32 +0900

    Implement in_wannaHit(), in_isUser()

--- src/main/java/com/cnu/blackjack/AppIO.java
+++ src/main/java/com/cnu/blackjack/AppIO.java
@@ -1,33 +1,61 @@
 package com.cnu.blackjack;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class AppIO {
-    public static boolean AppIO_in_yesOrNo(String answer) {
-        System.out.println("yes or no : ");
-        if(answer.equals("yes")) {
+
+    public static void out_message(String message) {
+        System.out.println(message);
+    }
+
+    public static void out_errorMessage(String message) {
+        System.out.println("[오류] " + message);
+    }
+
+    private static String in_line(String message) {
+        System.out.print(message);
+
+        Scanner scan = new Scanner(System.in);
+        String answer = scan.nextLine();
+        scan.close();
+
+        return answer;
+    }
+
+    private static boolean in_confirm(String message) throws YesOrNoException {
+        String answer = AppIO.in_line(message);
+
+        if (answer.equals("y")) {
             return true;
-        }else if (answer.equals("no")){
+        }
+        else if (answer.equals("n")) {
             return false;
-        }else{
+        }
+        else {
             throw new YesOrNoException();
         }
     }
 
-    public static String AppIO_in_String(){
-        return new Scanner(System.in).next();
-    }
-
-    public static int AppIO_in_numberOfPlayers(int numberOfPlayers) {
-        System.out.println("플레이어 수를 입력하시오 : ");
-        if(numberOfPlayers < 7 && numberOfPlayers >0){
-            return numberOfPlayers;
-        }else{
-            throw new NumberOfPlayersException();
+    public static boolean in_wannaHit() {
+        while (true) {
+            try {
+                return AppIO.in_confirm("Hit 하시겠습니까? [y/n]: ");
+            }
+            catch (YesOrNoException e) {
+                AppIO.out_errorMessage("잘못된 입력입니다.");
+            }
         }
     }
 
-    public static int AppIO_in_Integer(){
-        return new Scanner(System.in).nextInt();
+    public static boolean in_isUser() {
+        while (true) {
+            try {
+                return AppIO.in_confirm("사용자 입니까? [y:사용자 / n:컴퓨터]: ");
+            }
+            catch (YesOrNoException e) {
+                AppIO.out_errorMessage("잘못된 입력입니다.");
+            }
+        }
     }
 }
